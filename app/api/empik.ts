22c197c3a6453ab0186d4310e134dd0a7e585a24
Paweł Philipczyk <pawel.philipczyk@cloudinary.com>
2022-08-19 22:56:26 +0200

    add books fetching api and loader

---
+++ app/api/empik.ts
@@ -0,0 +1,57 @@
+import { load } from "cheerio";
+const feed = "https://www.empik.com/bestsellery/ksiazki";
+
+const getPageUrl = (start: string = "0") =>
+  feed +
+  "?" +
+  new URLSearchParams({
+    searchCategory: "31",
+    hideUnavailable: "false",
+    start,
+    resultsPP: "60",
+  });
+
+type Book = {
+  title: string;
+  author: string;
+  image: string;
+  price: string;
+  score: string;
+  url: string;
+};
+
+async function fetchPage(url: string) {
+  return fetch(url).then((response) => response.text());
+}
+
+function parsePage(html: string) {
+  const books: Partial<Book>[] = [];
+  const $ = load(html);
+
+  $(".search-list-item").each((i, elem) => {
+    const [, title] = $(".ta-product-title", elem).text().split("\n");
+    const author = $(".ta-product-smartauthor", elem).text().trim();
+    const image = $("img", elem).attr("lazy-img");
+    const score = $(".score", elem).text().trim();
+    const url = $("a", elem).attr('href');
+    const price = $(".ta-price-tile", elem)
+      .text()
+      .split("\n")
+      .filter(Boolean)
+      .shift();
+    books.push({ title, author, image, price, score, url });
+  });
+
+  return books;
+}
+
+export async function fetchBestsellerBooks() {
+  const urls = [undefined, "61"].map(getPageUrl);
+
+  try {
+    const pages = await Promise.all(urls.map(fetchPage));
+    return pages.map(parsePage).flat();
+  } catch (error) {
+    return { error };
+  }
+}
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,4 +1,15 @@
+import { useLoaderData } from "@remix-run/react";
+import { json } from "@remix-run/server-runtime";
+import { fetchBestsellerBooks } from "~/api/empik";
+
+export async function loader() {
+  const books = await fetchBestsellerBooks();
+  return json({ books });
+}
+
 export default function Index() {
+  const data = useLoaderData();
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Empik Bestsellers</h1>
@@ -27,6 +38,7 @@ export default function Index() {
           </a>
         </li>
       </ul>
+      <pre>{JSON.stringify(data?.books, undefined, 2)}</pre>
     </div>
   );
 }
