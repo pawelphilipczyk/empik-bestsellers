296a336cef2ff28084dab46c67972a37321fc30e
Paweł Philipczyk <pawel.philipczyk@cloudinary.com>
2022-08-27 07:27:16 +0200

    add global table styles

--- app/root.tsx
+++ app/root.tsx
@@ -7,12 +7,17 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import styles from "~/styles/global.css";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "Top Książki",
   viewport: "width=device-width,initial-scale=1",
 });
+
+export function links() {
+  return [{ rel: "stylesheet", href: styles }];
+}
 
 export default function App() {
   return (
