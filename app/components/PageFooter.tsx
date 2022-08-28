660770e06ce7bac213cf8baa3175ed37f1483fb3
Paweł Philipczyk <pawel.philipczyk@cloudinary.com>
2022-08-28 20:44:07 +0200

    update site footer

--- app/components/PageFooter.tsx
+++ app/components/PageFooter.tsx
@@ -1,21 +1,9 @@
-import type { HTMLAttributes} from 'react';
+import type { HTMLAttributes } from "react";
 
-type Props = HTMLAttributes<HTMLDivElement>
+type Props = HTMLAttributes<HTMLDivElement>;
 
 export const PageFooter = (props: Props) => (
   <footer {...props}>
-    <nav>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://www.empik.com/bestsellery/ksiazki"
-            rel="noreferrer"
-          >
-            TOP 100 Books
-          </a>
-        </li>
-      </ul>
-    </nav>
+    <h6>© Paweł Philipczyk</h6>
   </footer>
 );
